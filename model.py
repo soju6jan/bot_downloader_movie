d2caef118d9b8d486161207dac0ae243746d71ae
soju6jan <cybersol@naver.com>
2020-08-29 20:50:37 +0900

    1

--- model.py
+++ model.py
@@ -423,6 +423,8 @@ class ModelMovieItem(db.Model):
             
             if entity is not None:
                 #logger.debug(entity)
+                if entity.folderid != '':
+                    return True
                 entity.folderid = data['folderid']
                 entity.folderid_time = datetime.now()
                 db.session.commit()
